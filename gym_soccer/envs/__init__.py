aee40308c91501241ccfd2d5abd0a5fc2a1be425
leadfeet123 <kigango123@gmail.com>
2019-01-14 20:59:41 -0500

    Update __init__.py

--- gym_soccer/envs/__init__.py
+++ gym_soccer/envs/__init__.py
@@ -1,3 +1,1 @@
-from gym_soccer.envs.soccer_env import SoccerEnv
-from gym_soccer.envs.soccer_empty_goal import SoccerEmptyGoalEnv
-from gym_soccer.envs.soccer_against_keeper import SoccerAgainstKeeperEnv
+from gym_psched.envs.psched_env import PschedEnv
